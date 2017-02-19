72f755afdb5498651c2243d9787881e9f3e2b658
Duc Truong <phuduc1193@gmail.com>
2017-02-18 18:42:05 -0600

    Dynamically output menu from json file

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -4,10 +4,12 @@ import {
 } from '../builders';
 import navbar from './navbar';
 import banner from './banner';
+import menu from './menu';
 
-export default function app() {
+export default function app(menuJson) {
     const navbarEle = navbar();
     const bannerEle = banner();
+    const menuEle = menu(menuJson);
     const appEle = addId(div(navbarEle, bannerEle, menuEle), 'app-container');
 
     return appEle;
